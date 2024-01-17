6510c7b5d0391cc49af6aad1895c7354c9026980
Drazen Dodik <drazend@live.fi>
2024-01-17 16:59:04 +0200

    Run in single job

--- train.py
+++ train.py
@@ -117,14 +117,13 @@ optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
 # Decay LR by a factor of 0.1 every 7 epochs
 exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
 
-if __name__ == '__main__':
-    # Get a batch of training data
-    inputs, classes = next(iter(dataloaders['train']))
+# Get a batch of training data
+inputs, classes = next(iter(dataloaders['train']))
 
-    # Make a grid from batch
-    out = torchvision.utils.make_grid(inputs)
+# Make a grid from batch
+out = torchvision.utils.make_grid(inputs)
 
-    model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
-                        num_epochs=5)
-    
-    torch.save(model_ft, "output/model.pth")
+model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
+                    num_epochs=5)
+
+torch.save(model_ft, "output/model.pth")
